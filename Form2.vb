cbff3b1cbe7d3f7d5d907216517965bbcf8a60cf
raaaaaahulll <166096293+raaaaaahulll@users.noreply.github.com>
2024-05-16 10:09:20 +0530

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "enter name"
+        Label2.Text = "rollno"
+        Button1.Text = "submit"
+        Label3.Text = "Name"
+        Label4.Text = "rollno"
+        Button2.Text = "submit"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As String
+        n = TextBox1.Text
+        Label3.Text = n
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim a As Integer
+        a = TextBox2.Text
+        Label4.Text = a
+    End Sub
+
+End Class
